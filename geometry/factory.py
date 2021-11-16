fe577c09b50499dcc454cb4dc6b064f3c5222c73
Thomas David <thomasdavid0@gmail.com>
2021-11-16 16:25:40 +0000

    factory methods

--- geometry/factory.py
+++ geometry/factory.py
@@ -16,9 +16,15 @@ def geoms_factory(name, names, Single):
             columns=[prefix + col + suffix for col in columns],
             index=index
         ) 
-    
+
+    def __dir__(self):
+        return self.super().__dir__() + names
+
     def __getitem__(self, i):
         return Single(*self.data[i,:])
+
+    def count(self):
+        return self.data.shape[0]
 
     Obj =  type(name, (object,),
         {   
@@ -26,7 +32,9 @@ def geoms_factory(name, names, Single):
             "__init__": __init__,
             "__getattr__": __getattr__,
             "__getitem__": __getitem__,
+            "__dir__": __dir__,
             "to_pandas": to_pandas,
+            "count": property(count)
         })
     
     Obj.from_pandas = staticmethod(lambda df: Obj(np.array(df)))
@@ -45,12 +53,19 @@ def geom_factory(name, names):
     def to_dict(self, prefix=""):
         return {prefix + key: value for key, value in self.__dict__.items()}
         
+    def to_list(self):
+        return [getattr(self, key, None) for key in names]
+
+    def __dir__(self):
+        return self.super().__dir__() + names
 
     Obj =  type(name, (object,),
         {
             "names": names,
             "__init__": __init__,
+            "__dir__": __dir__,
             "to_dict": to_dict,
+            "to_list": to_list,
         })
 
     
