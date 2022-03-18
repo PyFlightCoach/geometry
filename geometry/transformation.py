437ef4c376354609610b227aa207cacc2c56204f
Thomas David <thomasdavid0@gmail.com>
2022-03-18 13:38:13 +0000

    transformation init bug

--- geometry/transformation.py
+++ geometry/transformation.py
@@ -22,7 +22,7 @@ class Transformation(Base):
         if len(args) == len(kwargs) == 0:
             args = np.concatenate([P0().data,Q0().data],axis=1)
         if len(args) == 2:
-            args = np.concatenate(args[0].data, args[1].data, axis=1)
+            args = np.concatenate([args[0].data, args[1].data], axis=1)
 
         super().__init__(*args, **kwargs)
         self.p = Point(self.data[:,:3])
