36b821aaa066dbcdc36c9f2b0cba94e9d2b8dada
Thomas David <thomasdavid0@gmail.com>
2025-07-19 21:48:44 +0100

    coord plot line width

--- src/geometry/coordinate_frame.py
+++ src/geometry/coordinate_frame.py
@@ -88,7 +88,7 @@ class Coord(Base):
     def axes(self):
         return Point.concatenate([self.x_axis, self.y_axis, self.z_axis])
 
-    def plot(self, fig=None, scale=1, label: str = None):
+    def plot(self, fig=None, scale=1, width=2, label: str = None):
         import plotly.graph_objects as go
         if fig is None:
             fig = go.Figure(layout=dict(scene=dict(aspectmode="data")))
@@ -116,7 +116,7 @@ class Coord(Base):
                     z=[self.origin.z[0], (self.origin.z + axis.z * scale)[0]],
                     mode="lines",
                     name=f"{label or 'Axis'} {Point.cols[i]}",
-                    line=dict(width=2, color=colors.pop(0))
+                    line=dict(width=width, color=colors.pop(0))
                 )
             )
         return fig
